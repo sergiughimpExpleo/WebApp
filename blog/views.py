a447446f7f84c734d8fc0b5396bdb96ac2698eaf
sergiughimpExpleo <78013000+sergiughimpExpleo@users.noreply.github.com>
2021-02-12 13:32:47 +0200

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+# Create your views here.
+
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+
+]
+
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
