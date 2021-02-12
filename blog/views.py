a94bdb51b401bf36ace9919ec8d6c4366d6d5e28
sergiughimpExpleo <78013000+sergiughimpExpleo@users.noreply.github.com>
2021-02-12 17:07:18 +0200

    admin page. database and migrations

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,27 @@
 from django.shortcuts import render
+from .models import Post
 # Create your views here.
 
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
+# posts = [
+#     {
+#         'author': 'CoreyMS',
+#         'title': 'Blog post 1',
+#         'content': 'First post content',
+#         'date_posted': 'August 27, 2018'
+#     },
+#     {
+#         'author': 'Jane Doe',
+#         'title': 'Blog post 2',
+#         'content': 'Second post content',
+#         'date_posted': 'August 28, 2018'
+#     }
 
-]
+# ]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
